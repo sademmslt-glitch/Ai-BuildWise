dee1007940d35b0c036a2e7ca210e48032dbb3b2
sademmslt-glitch <sademmslt@gmail.com>
2025-12-16 06:41:47 +0300

    Update predict_logic.py

--- predict_logic.py
+++ predict_logic.py
@@ -14,7 +14,7 @@ def safe_load_model(path):
     except Exception:
         return None
 
-cost_model = safe_load_model("cost_model_columns.pkl")
+cost_model = safe_load_model("cost_model_columns.pk1")
 delay_model = safe_load_model("delay_prediction_model.pkl")
 model_columns = safe_load_model("model_columns.pkl")
 
